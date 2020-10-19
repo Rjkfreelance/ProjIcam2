bfefe79bd89e68c351c90063752ef7dc0d483de0
Rjkfreelance <phpteach@outlook.com>
2020-10-19 08:12:21 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -10,10 +10,27 @@ Stream = require('node-rtsp-stream');
   wsPort: 9999,
   ffmpegOptions: { // options ffmpeg flags
     '-r': 25,// options with required values specify the value after the key
-    '-q:v':32
-    //'-s':'1280x720'
+    '-q:v':32,
+    '-s':'1280x720'
   }
 });
+app.get('/',(req, res) =>
+res.send(`
+  
+  <canvas id="videoCanvas1"  style="display:block;width:70%;height:70%;margin:0"></canvas>
 
+  <script src='https://cdnjs.cloudflare.com/ajax/libs/jsmpeg/0.2/jsmpg.min.js'></script>
+  <script>
+  var canvas1 = document.getElementById('videoCanvas1');
+   var ws1 = new WebSocket("ws://localhost:9999")
+     
+        var player1 = new jsmpeg(ws1, {canvas:canvas1, autoplay:true,audio:false,loop: true});
+       var canvas1 = document.getElementById('videoCanvas1');
+    
+    
+  </script>
+`),
+
+)
 console.log("Server Start on port: 8000")
 app.listen(process.env.PORT||8000);
